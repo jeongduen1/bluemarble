a4145f9f9415498d8ba2656c091bf7179acec1dc
jeongduen1 <147616300+jeongduen1@users.noreply.github.com>
2023-11-18 01:56:03 +0900

    asdasdasdasd
    asdfasasdfasdf

--- bluemarble.py
+++ bluemarble.py
@@ -43,16 +43,27 @@ MenuExitButton_pos2 = (width/2+200,height/2+350)
 RuleNotePage1 = pygame.image.load("images/UI/RuleNotePage1.png").convert()
 RuleNotePage2 = pygame.image.load("images/UI/RuleNotePage2.png").convert() 
 RuleNotePage3 = pygame.image.load("images/UI/RuleNotePage3.png").convert()
-RuleNotePage3 = pygame.image.load("images/UI/RuleNotePage4.png").convert() 
+RuleNotePage4 = pygame.image.load("images/UI/RuleNotePage4.png").convert()
+NextPageButton = pygame.image.load("images/buttons/NextPageButton.png").convert()
+NextPageButton = pygame.transform.scale(NextPageButton,(80,80))
+NextPageButton.set_colorkey(White)
+NextPageButton_pos1 = (1150,height/2 - 40)
+NextPageButton_Cpos1 = (1190,height/2)
+NextPageButton_pos2 = (1350,height/2 - 40)
+NextPageButton_Cpos2 = (1390,height/2)
+BackMenuButton_pos1 = (753,384)
+BackMenuButton_pos2 = (1252,516)
+BackPage1Button_Cpos = (1257,784)
+
 #GameLoop
 MenuWorking = True
-MenuEventType = 1
+Eventype = 1
 while MenuWorking :
     MenuCase1Working = True
     MenuCase2Working = True
     MenuCase3Working = True
     MenuCase4Working = True
-    match MenuEventType :
+    match Eventype :
         #Menu
         case 1:
             while MenuCase1Working :
@@ -64,12 +75,12 @@ while MenuWorking :
                         #RuleButton
                         if isClickButton(GameRuleButton_pos1,GameRuleButton_pos2,Click_pos):
                             MenuCase1Working = False
-                            MenuEventType = 2 #->GameRule
+                            Eventype = 2 #->GameRule
                             print('Menu to GameRule')
                         #StartButton
                         if isClickButton(MenuStartButton_pos1,MenuStartButton_pos2,Click_pos):
                             MenuCase1Working = False
-                            MenuEventType = 3 #->Game
+                            Eventype = 3 #->Game
                             print('Menu to Game')
                         if isClickButton(MenuExitButton_pos1,MenuExitButton_pos2,Click_pos):
                             sys.exit()
@@ -88,15 +99,33 @@ while MenuWorking :
                         sys.exit()
                     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 :
                         Click_pos = pygame.mouse.get_pos()
-                
-                screen.fill(Black)
+                        print(Click_pos)
+                        match page :
+                            case 1 :
+                                if distance(NextPageButton_Cpos1,Click_pos) <= 40 :
+                                    page += 1
+                            case 2 :
+                                if distance(NextPageButton_Cpos2,Click_pos) <= 40 :
+                                    page += 1
+                            case 3 :
+                                if distance(NextPageButton_Cpos2,Click_pos) <= 40 :
+                                    page += 1
+                            case 4 :
+                                if distance(BackPage1Button_Cpos,Click_pos) < 40:
+                                    page = 1
+                                if isClickButton(BackMenuButton_pos1,BackMenuButton_pos2,Click_pos) :
+                                    Eventype = 1
+                                    MenuCase2Working = False
                 match page :
                     case 1:
                         screen.blit(RuleNotePage1,(0,0))
+                        screen.blit(NextPageButton,NextPageButton_pos1)
                     case 2:
                         screen.blit(RuleNotePage2,(0,0))
+                        screen.blit(NextPageButton,NextPageButton_pos2)
                     case 3:
                         screen.blit(RuleNotePage3,(0,0))
+                        screen.blit(NextPageButton,NextPageButton_pos2)
                     case 4:
                         screen.blit(RuleNotePage4,(0,0))
                 pygame.display.update()
@@ -106,7 +135,7 @@ while MenuWorking :
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         sys.exit()
-                screen.fill(while)
+                screen.fill(White)
                 pygame.display.update()
         #EndGame
         case 4 :
