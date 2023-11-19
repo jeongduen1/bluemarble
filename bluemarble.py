130558b592784b3e7085c34bf326f2b95c72e60c
jeongduen1 <147616300+jeongduen1@users.noreply.github.com>
2023-11-19 11:23:11 +0900

    dfdfdfdfdf
    ddfdfdffddfdf

--- bluemarble.py
+++ bluemarble.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 #Colors
 White = (255,255,255)
+BackGroundColor = (125,178,73)
 Black = (0,0,0)
 #Classes
 
@@ -28,6 +29,7 @@ def isClickButton(BtnPos1,BtnPos2,ClickPos) :
         return True
     else :
         return False
+
 def teamUIBlit(num) :
     match num :
         case 2:
@@ -62,7 +64,7 @@ def MyTurnMakerDraw(num):
                 screen.blit(MyTurnMarker,TurnMarker_GreenUIpos)
             case 4 :
                 screen.blit(MyTurnMarker,TurnMarker_YellowUIpos)
-#Menu,GameRule,Game,EndGame
+#Menu,GameRule,GameNumber,Game,EndGame
 #Menu Images
 GameMenuBoard = pygame.image.load("images/UI/GameMenu.png").convert()
 GameMenuBoard_pos = (0,0)
@@ -95,7 +97,40 @@ BackMenuButton_pos1 = (525, 365)
 BackMenuButton_pos2 = (1074, 468)
 BackPage1Button_pos1 = (525, 498)
 BackPage1Button_pos2 = (1073, 599)
-
+#Game Images
+#Map Image
+mapImage = pygame.image.load("images/board/mapimage.png").convert()
+mapImage = pygame.transform.scale(mapImage,(900,900))
+mapImage_pos = (350,0)
+#redteam UI
+RedTeamTurnText = pygame.image.load("images/UI/RedTeamTurnText.png").convert()
+RedTeamTurnText.set_colorkey(White)
+RedTeamUI = pygame.image.load("images/UI/RedTeamUI.png").convert()
+RedTeamUI = pygame.transform.scale(RedTeamUI,(350,200))
+#blueteam UI
+BlueTeamTurnText = pygame.image.load("images/UI/BlueTeamTurnText.png").convert()
+BlueTeamTurnText.set_colorkey(White)
+BlueTeamUI = pygame.image.load("images/UI/BlueTeamUI.png").convert()
+BlueTeamUI = pygame.transform.scale(BlueTeamUI,(350,200))
+#greenteam UI
+GreenTeamTurnText = pygame.image.load("images/UI/GreenTeamTurnText.png").convert()
+GreenTeamTurnText.set_colorkey(White)
+GreenTeamUI = pygame.image.load("images/UI/GreenTeamUI.png").convert()
+GreenTeamUI = pygame.transform.scale(GreenTeamUI,(350,200))
+#yellowteam UI
+YellowTeamTurnText = pygame.image.load("images/UI/YellowTeamTurnText.png").convert()
+YellowTeamTurnText.set_colorkey(White)
+YellowTeamUI = pygame.image.load("images/UI/YellowTeamUI.png").convert()
+YellowTeamUI = pygame.transform.scale(YellowTeamUI,(350,200))
+#차례표식
+TurnMarker = pygame.image.load("images/markers/TurnMarker.png").convert()
+TurnMarker = pygame.transform.scale(TurnMarker,(65,65))
+MyTurnMarker = pygame.image.load("images/markers/MyTurnMarker.png").convert()
+MyTurnMarker = pygame.transform.scale(MyTurnMarker,(65,65))
+TurnMarker_RedUIpos = (73,15)
+TurnMarker_BlueUIpos = (73,225)
+TurnMarker_GreenUIpos = (73,435)
+TurnMarker_YellowUIpos = (73,645)
 #GameLoop
 MenuWorking = True
 Eventype = 1
@@ -104,6 +139,7 @@ while MenuWorking :
     MenuCase2Working = True
     MenuCase3Working = True
     MenuCase4Working = True
+    MenuCase5Working = True
     match Eventype :
         #Menu
         case 1:
@@ -123,11 +159,10 @@ while MenuWorking :
                         if isClickButton(MenuStartButton_pos1,MenuStartButton_pos2,Click_pos):
                             MenuCase1Working = False
                             Eventype = 3 #->Game
-                            print('Menu to Game')
+                            print('Menu to SelectGameNumber')
                         #GameExitButton
                         if isClickButton(MenuExitButton_pos1,MenuExitButton_pos2,Click_pos):
                             sys.exit() #-> GameExit
-                screen.fill(White)
                 screen.blit(GameMenuBoard,GameMenuBoard_pos)
                 pygame.display.update()
         #GameRule
@@ -169,17 +204,26 @@ while MenuWorking :
                     case 4:
                         screen.blit(RuleNotePage4,(0,0))
                 pygame.display.update()
-        #Game
-        case 3 :
+        #GameNumber
+        case 3:
             while MenuCase3Working :
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         sys.exit()
-                screen.fill(White)
-                pygame.display.update()
-        #EndGame
+                
+        #Game
         case 4 :
             while MenuCase4Working :
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+                screen.fill(BackGroundColor)
+                screen.blit(mapImage,mapImage_pos)
+                teamUIBlit()
+                pygame.display.update()
+        #EndGame
+        case 5 :
+            while MenuCase5Working :
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         sys.exit()
